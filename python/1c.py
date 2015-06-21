baa635c817dffcce0f3c7fcb837e9f46619a30c9
hauke-kiel <hdw@gmx.net>
2015-06-21 14:36:01 +0200

    Berechnung für 1c und 1b ist bewisen

--- python/1c.py
+++ python/1c.py
@@ -14,32 +14,40 @@ x = np.array([x1, x2])
 
 m = []
 
-##nM.append(m1)
-##nM.append(m1)
+##nm.append(m1)
+##nm.append(m1)
 
-#nM = np.append(nM,m1,0)
-#nM = np.append(nM,m2,0)
+#nm = np.append(nm,m1,0)
+#nm = np.append(nm,m2,0)
 
-##print nM
+##print nm
 ##print np.sum(x1, axis=0,dtype=np.float)/x1.shape
 for i in range(x.shape[0]):
     m.append(np.sum(x[i], axis=0,dtype=np.float)/x[i].shape)
 
-##print np.hstack((nM, np.atleast_2d(m1).T))
+##print np.hstack((nm, np.atleast_2d(m1).t))
 
 ##print np.hstack(m1,m2)
 
-##print nM
+print m
 
 #print m1
 #print x1
 #print (x1 - m1)
 
+sum = np.zeros(shape=(x.shape[2],x.shape[2]))
 for k in range(x.shape[0]):
-    sumx1 = 0
     for n in range(x.shape[1]):
-        ##print x[k][m]-m[k]
-        sumx1 = sumx1 + np.dot((x[k])[n]-m[k],(x[k])[n]-m[k])
-    print sumx1
+        print np.dot(np.transpose((x[k])[n]),(x[k])[n])
+        z = np.dot(np.transpose((x[k])[n]-m[k]), (x[k])[n]-m[k])
+        print z
+        sum = sum + z
+sum = sum / (x.shape[1])
+print sum
 
 #print np.dot(x1 - m1, x1-m1)
+
+
+a = np.array( ((2,3)) )
+b = np.array( ((2),(3)))
+print a*b
