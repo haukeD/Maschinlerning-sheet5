850d2caca03cd194e4c711f8b0c0e1c2bc1c8d98
hauke-kiel <hdw@gmx.net>
2015-06-22 15:00:07 +0200

    1c Transformation der Punkte fehlt, ob w stimmt überprüfe ich noch mal mit Python

--- python/1c.py
+++ python/1c.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#start
 x1 = np.array([[4,1],[2,4],[2,3],[3,6],[4,4]])
 x2 = np.array([[9,10],[6,8],[9,5],[8,7],[10,8]])
 
 x = np.array([x1, x2])
+
+#calc meanValues
+for i in range(x.shape[0]):
+    m.append(np.sum(x[i], axis=0,dtype=np.float)/x[i].shape)
+
+print m
 
 ##m1 = np.sum(x1, axis=0,dtype=np.float)/x1.shape
 ##m2 = np.sum(x2, axis=0,dtype=np.float)/x2.shape
@@ -22,8 +29,7 @@ m = []
 
 ##print nm
 ##print np.sum(x1, axis=0,dtype=np.float)/x1.shape
-for i in range(x.shape[0]):
-    m.append(np.sum(x[i], axis=0,dtype=np.float)/x[i].shape)
+
 
 ##print np.hstack((nm, np.atleast_2d(m1).t))
 
